93b7661016cc2c9420872486dbd1caa95c1a1a2d
kevindexter <kdexter@gac.edu>
2014-02-11 13:54:24 -0600

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package mcs270.helloWorld;
+import java.io.*;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("We made a change!");
+	}
+
+}
